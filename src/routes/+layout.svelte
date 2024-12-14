750ff2c08b94e2787a9529b394da3920890c4804
epic-person-on yes <ryanh41137@gmail.com>
2024-12-14 09:33:44 +0000

    Fix the Fix for speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.css';
 	let { children } = $props();
+	injectSpeedInsights();
 </script>
 
 {@render children()}
 
-<injectSpeedInsights />
