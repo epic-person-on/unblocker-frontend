3f4ed57daeb48c3f1b0e79bc0a35b6d92d884722
epic-person-on yes <ryanh41137@gmail.com>
2024-12-14 04:23:51 +0000

    Vercel Speed Insight

--- src/lib/scriptinjection.svelte
+++ src/lib/scriptinjection.svelte
@@ -1,12 +1,13 @@
 <script>
 </script>
 <svelte:head>
+  
   <meta charset="UTF-8">
   <meta name="description" content="Coding Tutorials">
   <meta name="keywords" content="Sveltekit, Tailwind CSS, JavaScript">
   <meta name="author" content="epic-person-on">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/x-icon" href="favicon.png">
   <title>Coding Tutorials</title>
 </svelte:head>
 
